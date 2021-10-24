824e23e2f6b222c4be094ee26d2787bad5efc0c1
Renato Vicente <rntvicente@gmail.com>
2021-10-24 11:57:53 -0300

    feat: criação da interface Player

---
+++ src/players/interfaces/players.interface.ts
@@ -0,0 +1,9 @@
+export interface Player {
+  readonly id: string;
+  readonly phoneNumber: string;
+  readonly email: string;
+  name: string;
+  ranking: string;
+  position: number;
+  photo: string;
+}
