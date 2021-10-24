eb5e8d859a42c3aa6ee12c1d3b4380331b71ff24
Renato Vicente <rntvicente@gmail.com>
2021-10-24 13:19:39 -0300

    feat: injetado o service PlayersService

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,11 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 
 import { CreatePlayerDto } from './dtos/create-players.dto';
+import { PlayersService } from './players.service';
 
 @Controller('api/players')
 export class PlayersController {
+  private readonly playersService: PlayersService;
+
+  constructor(playersService: PlayersService) {
+    this.playersService = playersService;
+  }
+
   @Post()
-  async upsertPlayers(@Body() { email }: CreatePlayerDto) {
-    return { email };
+  async upsertPlayers(@Body() { name, email, phoneNumber }: CreatePlayerDto) {
+    this.playersService.upsertPlayer({ name, email, phoneNumber });
   }
 }
