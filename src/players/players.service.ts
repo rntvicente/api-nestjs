36be98391e5aef7cfc1c71b5fe03aee5a571cdbf
Renato Vicente <rntvicente@gmail.com>
2021-10-24 13:20:24 -0300

    feat: deve gravar um jogador quando chamado metodo UpsertPlayer

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+
+import { CreatePlayerDto } from './dtos/create-players.dto';
+import { Player } from './interfaces/players.interface';
 
 @Injectable()
-export class PlayersService {}
+export class PlayersService {
+  private readonly logger = new Logger(PlayersService.name);
+  private players: Player[] = [];
+
+  async upsertPlayer({
+    name,
+    email,
+    phoneNumber,
+  }: CreatePlayerDto): Promise<void> {
+    this.upsertPlayer({ name, email, phoneNumber });
+  }
+
+  create({ name, email, phoneNumber }: CreatePlayerDto): void {
+    this.players.push({
+      name,
+      email,
+      phoneNumber,
+      id: uuidv4(),
+      ranking: 'A',
+      position: 1,
+      photo: 'https://www.bing.com/search?q=foto123.jpg',
+    });
+  }
+}
