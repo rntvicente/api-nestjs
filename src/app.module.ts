a0beed9ffe003ce674b943f4c6e80053727fdc98
Renato Vicente <rntvicente@gmail.com>
2021-10-25 21:33:48 -0300

    feat: conectando ao Atlas MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { PlayersModule } from './players/players.module';
 
+// eslint-disable-next-line prettier/prettier
+const uriMongo = 'mongodb+srv://admin:xP2j52h7LAwfGms9@cluster0.yqcau.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const opts = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 @Module({
-  imports: [PlayersModule],
+  imports: [PlayersModule, MongooseModule.forRoot(uriMongo, opts)],
   controllers: [],
   providers: [],
 })
