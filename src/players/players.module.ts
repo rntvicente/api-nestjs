8b6343cf18d8018b5b4e555f2932cf93648d706a
Renato Vicente <rntvicente@gmail.com>
2021-10-25 21:46:08 -0300

    feat: adicionado schema

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { PlayersController } from './players.controller';
 import { PlayersService } from './players.service';
+import { PlayersSchema } from './interfaces/players.schema'
 
+const features = [{ name: 'Players', schema: PlayersSchema }];
 @Module({
+  imports: [MongooseModule.forFeature(features)],
   controllers: [PlayersController],
   providers: [PlayersService],
 })
