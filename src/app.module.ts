784982ee6746ba314dc25db1e26a053b91dc1348
Renato Vicente <rntvicente@gmail.com>
2021-10-26 21:19:06 -0300

    refactor: alterado nome de database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
 // eslint-disable-next-line prettier/prettier
-const uriMongo = 'mongodb+srv://admin:xP2j52h7LAwfGms9@cluster0.yqcau.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const uriMongo = 'mongodb+srv://admin:xP2j52h7LAwfGms9@cluster0.yqcau.mongodb.net/api-smartranking?retryWrites=true&w=majority';
 const opts = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
