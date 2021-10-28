02f35dd4886d2a88e997363fc2d9e8d3145fafab
Renato Vicente <rntvicente@gmail.com>
2021-10-27 22:38:59 -0300

    feat: criado DTO para atualização

--- src/players/dtos/create-players.dto.ts
+++ src/players/dtos/create-players.dto.ts
@@ -1,5 +1,12 @@
+import { IsNotEmpty, IsEmail, IsMobilePhone } from 'class-validator';
+
 export class CreatePlayerDto {
+  @IsMobilePhone('pt-BR')
   readonly phoneNumber: string;
+
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly name: string;
 }
