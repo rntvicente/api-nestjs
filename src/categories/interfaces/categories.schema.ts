c8572010b0a6bfed1239eacb774524fe1140f9b1
Renato Vicente <rntvicente@gmail.com>
2021-11-01 19:35:58 -0300

    feat: schema CategorySchema

---
+++ src/categories/interfaces/categories.schema.ts
@@ -0,0 +1,22 @@
+import * as mongoose from 'mongoose';
+
+export const CategorySchema = new mongoose.Schema(
+  {
+    category: { type: String, unique: true },
+    description: String,
+    events: [
+      {
+        name: String,
+        operation: String,
+        value: Number,
+      },
+    ],
+    players: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Player',
+      },
+    ],
+  },
+  { timestamps: true, collection: 'categories' },
+);
