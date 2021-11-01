77b4e7ec5b89d047bf4f595c76a2d9b21a152c52
Renato Vicente <rntvicente@gmail.com>
2021-11-01 19:35:28 -0300

    feat: transfer de data de criação de categoria

---
+++ src/categories/dtos/create-categories.dto.ts
@@ -0,0 +1,27 @@
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsString,
+  Length,
+  ValidateNested,
+} from 'class-validator';
+
+import { Event } from '../interfaces/categories.interface';
+
+export class CreateCategoriesDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 1)
+  readonly category: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(10, 20)
+  description: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  events: Event[];
+}
