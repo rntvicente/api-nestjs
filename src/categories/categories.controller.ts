3c2210c32358987f627aa86f83b280341f5b4b6a
Renato Vicente <rntvicente@gmail.com>
2021-11-01 20:01:41 -0300

    feat: rota de atualização

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,13 +1,17 @@
 import {
   Body,
   Controller,
+  Param,
   Post,
+  Put,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 
 import { CategoriesService } from './categories.service';
 import { CreateCategoriesDto } from './dtos/create-categories.dto';
+import { UpdateCategoriesDto } from './dtos/update-categories.dto';
+import { Category } from './interfaces/categories.interface';
 
 @Controller('api/categories')
 export class CategoriesController {
@@ -24,6 +28,18 @@ export class CategoriesController {
   ) {
     return await this.categoriesService.CreateCategories({
       category,
+      description,
+      events,
+    });
+  }
+
+  @Put('/:category')
+  @UsePipes(ValidationPipe)
+  async updateCategory(
+    @Param('category') category: string,
+    @Body() { description, events }: UpdateCategoriesDto,
+  ): Promise<Category> {
+    return await this.categoriesService.UpdateCategoryByCategory(category, {
       description,
       events,
     });
